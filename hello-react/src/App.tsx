9a51cd94e2eead92f438791c81a929a1865d9470
Pavan Kumar <pavankittu19@gmail.com>
2023-08-21 17:24:02 +0530

    removed status and passed using dueDate prop

--- hello-react/src/App.tsx
+++ hello-react/src/App.tsx
@@ -14,16 +14,16 @@ function App() {
       <div className="grid grid-cols-5">
         <div className="TaskCell col-start-2 col-span-1 rounded">
           <h1 className="text-center text-gray-600 text-xl py-3 font-bold">Pending</h1>
-          <TaskCard title="Build the website with static content" dueDate="10th April" assigneeName="Pavan Kumar" statusOfTask="pending"/>
-          <TaskCard title="Add a blog" dueDate="18th April" assigneeName="Sandeep" statusOfTask="pending"/>
+          <TaskCard title="Build the website with static content" dueDate="10th April" assigneeName="Pavan Kumar" />
+          <TaskCard title="Add a blog" dueDate="18th April" assigneeName="Sandeep" />
           <div className="bg-gray-300 rounded px-2 py-1">
             <h3>+ New Task</h3>
           </div>
         </div>
         <div className="TaskCell col-start-4 col-span-1">
           <h1 className="text-center text-gray-600 text-xl py-3 font-bold">Done</h1>
-          <TaskCard title="Design the mockup" completedAtDate="10th May" assigneeName="Nithin" statusOfTask="completed"/>
-          <TaskCard title="Get the appproval from principal" completedAtDate="12th August" assigneeName="Vineeth" statusOfTask="completed" />
+          <TaskCard title="Design the mockup" completedAtDate="10th May" assigneeName="Nithin" />
+          <TaskCard title="Get the appproval from principal" completedAtDate="12th August" assigneeName="Vineeth" />
         </div>  
       </div>
     </div>
--- hello-react/src/TaskCard.tsx
+++ hello-react/src/TaskCard.tsx
@@ -4,25 +4,34 @@ import './TaskCard.css'
 
 interface taskcardProperties {
   title:string;
-  statusOfTask:string;
+  // statusOfTask:string;
   dueDate?:string;
   completedAtDate?:string;
   assigneeName:string;
 }
 const TaskCard : React.FC<taskcardProperties> = (props) => {
-  const status = props.statusOfTask
+  // const status = props.statusOfTask
   console.log(props)
+  if (props.dueDate) {
     return (
       <div className="TaskItem rounded-lg py-3 px-2">
         <h2 className="text-xl font-bold">{props.title}</h2>
-        {status === 'pending' ? (
-          <p>Due on: {props.dueDate}</p>
-        ):(
-          <p>Completed on: {props.completedAtDate}</p>
-        )}
+        <p>Due on: {props.dueDate}</p>
+        {/* <p>Completed on: {props.completedAtDate}</p> */}
         <p>Assignee: {props.assigneeName}</p>
       </div>
     )
+  }else{
+    return (
+      <div className="TaskItem rounded-lg py-3 px-2">
+        <h2 className="text-xl font-bold">{props.title}</h2>
+        {/* <p>Due on: {props.dueDate}</p> */}
+        <p>Completed on: {props.completedAtDate}</p>
+        <p>Assignee: {props.assigneeName}</p>
+      </div>
+    )
+  } 
+  
 }
 
 export default TaskCard
