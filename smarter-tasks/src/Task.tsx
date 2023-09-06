899711b06d04d604b08a3674c4b3173ffc592040
Pavan Kumar <pavankittu19@gmail.com>
2023-09-06 15:43:41 +0530

    l4

--- smarter-tasks/src/Task.tsx
+++ smarter-tasks/src/Task.tsx
@@ -1,20 +1,34 @@
-import React from "react";
+// import React from "react";
+import { TaskItem } from "./types";
+
 interface TaskProp {
-  title: string;
-  dueDate: string;
-  description: string;
+  item:TaskItem;
+  removeTask:(task:TaskItem) =>void
 }
-class Task extends React.Component<TaskProp> {
-  render() {
-    return (
-      <div className="TaskItem shadow-md border border-slate-100">
-        <h2 className="text-base font-bold my-1">{this.props.title}</h2>
-        <p className="text-sm text-slate-500">Due Date: {this.props.dueDate}</p>
-        <p className="text-sm text-slate-500">
-          Description: {this.props.description}
-        </p>
-      </div>
-    );
-  }
+// class Task extends React.Component<TaskProp> {
+//   render() {
+//     return (
+//       <div className="TaskItem shadow-md border border-slate-100">
+//         <h2 className="text-base font-bold my-1">{this.props.title}</h2>
+//         <p className="text-sm text-slate-500">Due Date: {this.props.dueDate}</p>
+//         <p className="text-sm text-slate-500">
+//           Description: {this.props.description}
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+const Task = (props:TaskProp) => {
+  const {item,removeTask} = props
+  return (
+    <div className="TaskItem shadow-md border border-slate-100">
+      <h2 className="text-base font-bold my-1">{item.title}</h2>
+      <p className="text-sm text-slate-500">{item.dueDate}</p>
+      <p className="text-sm text-slate-500">
+        Description: {item.description}
+      </p>
+      <button className="deleteTaskButton bg-gray-300 hover:bg-gray-600" onClick={()=>removeTask(item)}>Delete Task</button>
+    </div>
+  );
 }
 export default Task;
--- smarter-tasks/src/TaskList.tsx
+++ smarter-tasks/src/TaskList.tsx
@@ -1,25 +1,40 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React from "react";
+// import React from "react";
 import Task from "./Task";
 import { TaskItem } from "./types";
 
 interface Props {
   tasks: TaskItem[];
+  handleRemovedTask : (id:number) => void
 }
 
 interface State {}
 
-class TaskList extends React.Component<Props, State> {
-  render() {
-    return this.props.tasks.map((task, idx) => (
-      <Task
-        key={idx}
-        title={task.title}
-        dueDate={task.dueDate}
-        description={task.description}
-      />
-    ));
-  }
+// class TaskList extends React.Component<Props, State> {
+//   render() {
+//     return this.props.tasks.map((task, idx) => (
+//       <Task
+//         key={idx}
+//         title={task.title}
+//         dueDate={task.dueDate}
+//         description={task.description}
+//       />
+//     ));
+//   }
+// }
+
+const TaskList = (props:Props) =>{
+  const list = props.tasks.map((task,idx)=>(
+    <Task
+    key={idx}
+    // title={task.title}
+    // description={task.description}
+    // dueDate={task.dueDate}
+    item={task}
+    removeTask={()=>props.handleRemovedTask(idx)}
+    />
+  ))
+  return <>{list}</>; // shorthand notation for a react fragment
 }
 
 export default TaskList;
