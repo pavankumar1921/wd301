fdbc684d61c6325f01874065f60593f5276b6811
Pavan Kumar <pavankittu19@gmail.com>
2023-09-19 15:35:38 +0530

    added logout link

--- smarter-tasks/src/pages/dashboard/index.tsx
+++ smarter-tasks/src/pages/dashboard/index.tsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 const Dashboard: React.FC = () => {
-    const userData = JSON.parse(localStorage.getItem('userData'))
+    const navigate = useNavigate()
+    const userDataString = localStorage.getItem('userData')
+
+    const userData = userDataString? JSON.parse(userDataString) : null
+
+    const handleLogout = () =>{
+        localStorage.removeItem('authToken')
+        localStorage.removeItem('userData')
+        navigate('/signin')
+    }
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <h1 className="text-3xl font-bold text-center text-gray-800 mb-8">Dashboard</h1>
-      <h2>Name:{userData.name}</h2>
-    <h2>Email:{userData.email}</h2>
+    <div className="min-h-screen flex flex-col items-center justify-center">
+    <div className="max-w-md w-full px-6 py-8 bg-white rounded-lg text-center">
+      <h1 className="text-3xl font-bold text-gray-800 mb-8">Dashboard</h1>
+
+      {/* Display user data if it exists */}
+      {userData && (
+        <div className="mb-4">
+          <h2 className="text-lg font-semibold">Name: {userData.name}</h2>
+          <h2 className="text-lg font-semibold">Email: {userData.email}</h2>
+        </div>
+      )}
+      <a
+        id="logout-link"
+        href="#"
+        onClick={handleLogout}
+        className="text-blue-500 hover:underline cursor-pointer"
+      >
+        Logout
+      </a>
     </div>
+  </div>
     
   );
 }
