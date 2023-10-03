f2e19edfee1b0cfb2858c0d341ff573c32c9dca3
Pavan Kumar <pavankittu19@gmail.com>
2023-10-03 23:59:23 +0530

    added provider in app.tsx

--- smarter-tasks/src/App.tsx
+++ smarter-tasks/src/App.tsx
@@ -1,51 +1,3 @@
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-// } from "react-router-dom";
-// import Notfound from "./pages/Notfound";
-// import Signup from './pages/signup';
-// import Signin from "./pages/signin";
-// import Dashboard from "./pages/dashboard";
-// import ProtectedRoute from "./ProtectedRoute";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Signup />,
-//   },
-//   {
-//     path: "/signup",
-//     element: <Signup />,
-//   },
-//   {
-//     path: "/signin",
-//     element: <Signin/>,
-//   },
-//   {
-//     path: "/notfound",
-//     element: <Notfound />,
-//   },
-//   {
-//     path: "/dashboard",
-//     element: (
-//       <ProtectedRoute>
-//         <Dashboard/>
-//       </ProtectedRoute>
-//     )
-//   },
-//   {
-//     path: "*",
-//     element: <Notfound />,
-//   }
-// ]);
-
-// const App = () => {
-//   return (
-//     <RouterProvider router={router} />
-//   );
-// }
-
-// export default App
 import {useContext} from "react";
 import { RouterProvider } from "react-router-dom";
 import {ThemeContext} from "./context/theme";
@@ -53,6 +5,7 @@ import router from "./routes"
 
 import { ProjectsProvider } from "./context/projects/context";
 import { MembersProvider } from "./context/members/context";
+import { CommentProvider } from "./context/comment/context";
 
 const App = () => {
   const {theme} = useContext(ThemeContext)
@@ -60,7 +13,9 @@ const App = () => {
     <div className={`h-screen w-full mx-auto py-2 ${theme === "dark" ? "dark" : ""}`}>
       <ProjectsProvider>
         <MembersProvider>
-        <RouterProvider router={router} />
+          <CommentProvider>
+            <RouterProvider router={router} />
+          </CommentProvider>
         </MembersProvider>
       </ProjectsProvider>
     </div>
