b1a8e678a8eecd3f5c2effcac4cf3e9ca3a2d96c
Pavan Kumar <pavankittu19@gmail.com>
2023-10-09 23:28:40 +0530

    .

--- smarter-tasks/src/index.tsx
+++ smarter-tasks/src/index.tsx
@@ -2,9 +2,10 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-// import { ThemeProvider } from './context/theme.tsx'
+import { ThemeProvider } from './context/theme.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+    <ThemeProvider>
     <App/>
-
+    </ThemeProvider>
 )
